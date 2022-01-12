c179523e9f8bd5790ab4995845b7168641354cd4
Quynh <quynhdongikn@gmail.com>
2022-01-12 15:26:26 -0600

    mongodb connection

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 var path = require('path');
 require('dotenv/config');
 
-mongoose.connect(process.env.MONGO_URL,
+mongoose.connect("mongodb://localhost:27017/inventoryDB",
   { useNewUrlParser: true, useUnifiedTopology: true }, err => {
       console.log('connected')
   });
